6f0fcfa0eeb632f36bc7a516e8ebe5d5c219d8a1
WillST12 <willnerseverino343@gmail.com>
2025-07-17 11:55:48 -0400

    Labels de los campos de la base de datos modificados

--- Aplicacion Web. LogicOne/Models/TaskManagerDB.cs
+++ Aplicacion Web. LogicOne/Models/TaskManagerDB.cs
@@ -4,13 +4,13 @@ namespace Aplicacion_Web._LogicOne.Models
 {
     public class TaskManagerDB
     {
-        
+       
         public int ID { get; set; }
-        
+        [Display(Name = "Titulo De Tarea")]
         public string? TitleTask { get; set; }
-      
+        [Display(Name = "Descripcion")]
         public string? DescriptionTask { get; set; }
-
+        [Display(Name = "Estado (Completado/Pendiente)")]
         public bool Status { get; set; }
 
     }
